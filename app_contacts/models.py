462725f609b7d4616631c224245e6c014129f123
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-05-15 14:15:32 -0500

    contact models

--- app_contacts/models.py
+++ app_contacts/models.py
@@ -1,3 +1,21 @@
+# Django
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+
+    picture = models.ImageField(upload_to='contacts/picture')
+
+    firstname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30)
+
+    phone = models.CharField(max_length=30)
+    email = models.EmailField(max_length=100)
+
+    occupation = models.CharField(max_length=30)
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.firstname
