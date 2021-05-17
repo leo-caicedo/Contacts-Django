442b984c8636368be4fe000ff86051d555b445fc
Leonardo Caicedo <devleonardocaicedo@gmail.com>
2021-05-17 08:35:36 -0500

    fix model

--- app_contacts/models.py
+++ app_contacts/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class Contact(models.Model):
 
-    picture = models.ImageField(upload_to='contacts/picture')
+    picture = models.ImageField(upload_to='contacts/picture', blank=True, null=True)
 
     firstname = models.CharField(max_length=30)
-    lastname = models.CharField(max_length=30)
+    lastname = models.CharField(max_length=30, blank=True)
 
     phone = models.CharField(max_length=30, unique=True)
-    email = models.EmailField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True, blank=True)
 
-    occupation = models.CharField(max_length=30)
+    occupation = models.CharField(max_length=30, blank=True)
 
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
